6c4e838ef4c41c58083cb389e69f75c113eb752b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-06 15:53:11 -0300

    user managemment app

---
+++ users/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class SignUpPageTests(TestCase):
+
+    username = 'newuser'
+    email = 'newuser@email.com'
+
+    def setUp(self):
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+
+    def test_signup_page_works(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'account/signup.html')
+        self.assertContains(self.response, 'Signup')
+        self.assertNotContains(self.response, 'Hi there. I should not be here')
